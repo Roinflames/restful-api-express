b08c4044b280c4ed0336c27bd7253aa6430a4fc9
roinflames <rod.reyes.s@gmail.com>
2017-01-13 01:03:10 -0300

    rutas protegidas con jwt, no logro combinar node-restful con jwt

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express'),
     restful = require('node-restful'),
     mongoose = restful.mongoose;
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+var User = require('./app/models/user');
+var config = require('./config/main');
 
 var app = express();
 
@@ -40,9 +44,75 @@ var RaspberrySchema = mongoose.Schema({
   enabled: Boolean
 });
 
-var Users = restful.model('users', UserSchema);
-Users.methods(['get', 'put', 'post', 'delete']);
-Users.register(app, '/api/users');
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.get('/', function(req, res){
+  res.send("Aqui va la pagina de inicio.");
+});
+/*============================================================================*/
+//Rutas de la api
+var apiRoutes = express.Router();
+
+apiRoutes.post('/register', function(req,res){
+  if(!req.body.email || !req.body.password) {
+    res.json({ success: false, message: 'Porfavor ingrese email y contraseña.'});
+  } else {
+    var newUser = new User({
+      email: req.body.email,
+      password: req.body.password
+    });
+  //guardar usuario
+  newUser.save(function(err){
+    if (err) {
+      return res.json({success: false, message: 'El correo ya existe'});
+    }
+    res.json({ success: true, message: 'Usuario registrado con éxito.'});
+  });
+  }
+});
+
+//proteger una zona con jwt
+apiRoutes.get('/dashboard', passport.authenticate('jwt', {session: false}), function(req, res) {
+  res.send('Resulto! User id: '+ req.user._id + '.');
+});
+
+app.use('/api', apiRoutes);
+
+// ruta de registro de usuario para recibir jwt
+apiRoutes.post('/authenticate', function(req, res){
+  User.findOne({
+    email: req.body.email
+  }, function (err, user){
+    if (err) throw (err);
+
+    if(!user) {
+      res.send({ success: false, message: 'Fallo en la autenticación. Usuario no registrado.'});
+    } else {
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          var token = jwt.sign(user, config.secret, {
+            expiresIn: 10000 //segundos
+          });
+          res.json({ success: true, token: 'JWT '+ token});
+        } else {
+          res.send({ success: false, message: 'Fallo en la autenticación. La clave no coincide.'});
+        }
+      });
+    }
+  });
+});
+/*============================================================================*/
+//proteger una zona con jwt
+app.get('/api/users', passport.authenticate('jwt', {session: false}), function(req, res) {
+  res.send('Resulto! User id: '+ req.user._id + '.');
+});
+
+app.use('/api', apiRoutes);
+
+//var Users = restful.model('users', UserSchema);
+//Users.methods(['get', 'put', 'post', 'delete']);
+//Users.register(app, '/api/users');
 
 var Datalogs = restful.model('datalogs', DatalogSchema);
 Datalogs.methods(['get', 'put', 'post', 'delete']);
