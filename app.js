35b2609182b743d09568811dbc02a84403b26c1a
roinflames <rod.reyes.s@gmail.com>
2017-01-17 00:09:56 -0300

    get users y users/id con jwt

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ var passport = require('passport');
 var config = require('./config/main');
 var User = require('./app/models/user');
 var jwt = require('jsonwebtoken');
-var port = 3001;
+var port = process.env.PORT || 3001;
 
 // POST req para uso de la API
-app.use(bodyParser.urlencoded({ extended: false}));
+//TODO porque false o true?
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 //log req a la consola
@@ -27,6 +28,10 @@ require('./config/passport')(passport);
 
 //Rutas de la api
 var apiRoutes = express.Router();
+
+app.get('/', function(req, res){
+  res.json({welcome: "Bienvenido al API Energía. Encontrara las rutas en /api/<ruta>"});
+});
 
 apiRoutes.post('/register', function(req,res){
   if(!req.body.email || !req.body.password) {
@@ -49,12 +54,6 @@ apiRoutes.post('/register', function(req,res){
 //proteger una zona con jwt
 apiRoutes.get('/dashboard', passport.authenticate('jwt', {session: false}), function(req, res) {
   res.send('Resulto! User id: '+ req.user._id + '.');
-});
-
-app.use('/api', apiRoutes);
-
-app.get('/', function(req, res){
-  res.send("node-express-jwt home");
 });
 
 // ruta de registro de usuario para recibir jwt
@@ -80,6 +79,25 @@ apiRoutes.post('/authenticate', function(req, res){
     }
   });
 });
+
+apiRoutes.get('/users', passport.authenticate('jwt', {session: false}), function(req, res){
+  User.find(function(err, users){
+    if(err)
+      res.send(err);
+
+    res.json(users);
+  });
+});
+
+apiRoutes.get('/users/:user_id', passport.authenticate('jwt', {session: false}), function(req, res){
+        User.findById(req.params.user_id, function(err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    });
+
+app.use('/api', apiRoutes);
 
 app.listen(port, function(){
   console.log("Escuchando en el puerto 3001!");
